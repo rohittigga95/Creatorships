3c86ecb4f75ce7338d78a27e9f3ba8ae375ea2a0
rohittigga95 <rohit.yzx3135@gmail.com>
2024-07-16 20:52:31 +0530

    uploaded to vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import Hero from './Components/Hero';
 import { Footer } from './Components/Footer';
-import { Features } from './Components/Features';
-import { CtaComponent } from './Components/CtaComponent';
 import { Navbar } from './Components/Navbar';
-import { NewsLetter } from './Components/NewsLetter';
 import { Outlet } from 'react-router-dom';
 
 function App() {
