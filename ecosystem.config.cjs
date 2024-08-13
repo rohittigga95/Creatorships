712a47ec8b587fe89e96b00d18d5a7b46567ef11
Rohit Tigga <rohit.yzx3135@gmail.com>
2024-08-14 01:36:46 +0530

    port

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
         script: "serve",
         env: {
           PM2_SERVE_PATH: "./dist",
-          PM2_SERVE_PORT: 5173,
+          PM2_SERVE_PORT: 80,
           PM2_SERVE_SPA: "true",
           NODE_ENV: 'production',
         },
